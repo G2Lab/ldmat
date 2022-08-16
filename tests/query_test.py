cb88f912ed09beb172a6bbe24f8179b33441d3ac
Rockwell Weiner <rockwellw@gmail.com>
2022-08-16 14:35:01 -0400

    test streaming

--- tests/query_test.py
+++ tests/query_test.py
@@ -1,5 +1,8 @@
 import filecmp
+import os
 
+import numpy as np
+import scipy.sparse as sp
 from click.testing import CliRunner
 
 from src.ldmat.__main__ import submatrix, submatrix_by_maf
@@ -25,6 +28,8 @@ def test_npz_query():
 
     assert filecmp.cmp(tmp_output, "examples/query_result.npz")
 
+    os.remove(tmp_output)
+
 
 def test_csv_query():
     tmp_output = "/tmp/out.csv"
@@ -46,6 +51,8 @@ def test_csv_query():
 
     assert filecmp.cmp(tmp_output, "examples/query_result.csv")
 
+    os.remove(tmp_output)
+
 
 def test_maf_query():
     tmp_output = "/tmp/maf_out.npz"
@@ -58,3 +65,49 @@ def test_maf_query():
     assert result.exit_code == 0, print(result.output)
 
     assert filecmp.cmp(tmp_output, "examples/query_result_maf.npz")
+
+    os.remove(tmp_output)
+
+
+def test_streaming_query():
+    tmp_output = "/tmp/out.csv"
+
+    runner = CliRunner()
+    result = runner.invoke(
+        submatrix,
+        [
+            "examples/chr21_partial.h5",
+            "--i-start",
+            14900001,
+            "--i-end",
+            15100001,
+            "-o",
+            tmp_output,
+            "-s",
+        ],
+    )
+    assert result.exit_code == 0, print(result.output)
+
+    assert filecmp.cmp(tmp_output, "examples/query_result.csv")
+
+    os.remove(tmp_output)
+
+
+def test_streaming_maf_query():
+    tmp_output = "/tmp/maf_out.npz"
+
+    runner = CliRunner()
+    result = runner.invoke(
+        submatrix_by_maf,
+        ["examples/chr21_partial.h5", "-l", 0.2, "-u", 0.22, "-o", tmp_output, "-s"],
+    )
+    assert result.exit_code == 0, print(result.output)
+
+    test_output = np.nan_to_num(sp.load_npz(tmp_output).todense())
+    correct_output = np.nan_to_num(
+        sp.load_npz("examples/query_result_maf.npz").todense()
+    )
+
+    assert np.abs(test_output - correct_output).max() < 0.00001
+
+    os.remove(tmp_output)

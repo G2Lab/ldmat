7e28e52e977ae5b844dff3547eb1d47fa48521e1
Rockwell Weiner <rockwellw@gmail.com>
2022-08-29 14:28:38 -0400

    used proper tempfiles for tests

--- tests/query_test.py
+++ tests/query_test.py
@@ -1,5 +1,5 @@
 import filecmp
-import os
+from tempfile import NamedTemporaryFile
 
 import numpy as np
 import scipy.sparse as sp
@@ -9,105 +9,95 @@ from src.ldmat.__main__ import submatrix, submatrix_by_maf
 
 
 def test_npz_query():
-    tmp_output = "/tmp/out.npz"
+    with NamedTemporaryFile(suffix=".npz") as tmp:
 
-    runner = CliRunner()
-    result = runner.invoke(
-        submatrix,
-        [
-            "examples/chr21_partial.h5",
-            "--row-start",
-            14900001,
-            "--row-end",
-            15100001,
-            "-o",
-            tmp_output,
-        ],
-    )
-    assert result.exit_code == 0, print(result.output)
+        runner = CliRunner()
+        result = runner.invoke(
+            submatrix,
+            [
+                "examples/chr21_partial.h5",
+                "--row-start",
+                14900001,
+                "--row-end",
+                15100001,
+                "-o",
+                tmp.name,
+            ],
+        )
+        assert result.exit_code == 0, print(result.output)
 
-    assert filecmp.cmp(tmp_output, "examples/query_result.npz")
-
-    os.remove(tmp_output)
+        assert filecmp.cmp(tmp.name, "examples/query_result.npz")
 
 
 def test_csv_query():
-    tmp_output = "/tmp/out.csv"
+    with NamedTemporaryFile(suffix=".csv") as tmp:
 
-    runner = CliRunner()
-    result = runner.invoke(
-        submatrix,
-        [
-            "examples/chr21_partial.h5",
-            "--row-start",
-            14900001,
-            "--row-end",
-            15100001,
-            "-o",
-            tmp_output,
-        ],
-    )
-    assert result.exit_code == 0, print(result.output)
+        runner = CliRunner()
+        result = runner.invoke(
+            submatrix,
+            [
+                "examples/chr21_partial.h5",
+                "--row-start",
+                14900001,
+                "--row-end",
+                15100001,
+                "-o",
+                tmp.name,
+            ],
+        )
+        assert result.exit_code == 0, print(result.output)
 
-    assert filecmp.cmp(tmp_output, "examples/query_result.csv")
-
-    os.remove(tmp_output)
+        assert filecmp.cmp(tmp.name, "examples/query_result.csv")
 
 
 def test_maf_query():
-    tmp_output = "/tmp/maf_out.npz"
+    with NamedTemporaryFile(suffix=".npz") as tmp:
 
-    runner = CliRunner()
-    result = runner.invoke(
-        submatrix_by_maf,
-        ["examples/chr21_partial.h5", "-l", 0.2, "-u", 0.22, "-o", tmp_output],
-    )
-    assert result.exit_code == 0, print(result.output)
+        runner = CliRunner()
+        result = runner.invoke(
+            submatrix_by_maf,
+            ["examples/chr21_partial.h5", "-l", 0.2, "-u", 0.22, "-o", tmp.name],
+        )
+        assert result.exit_code == 0, print(result.output)
 
-    assert filecmp.cmp(tmp_output, "examples/query_result_maf.npz")
-
-    os.remove(tmp_output)
+        assert filecmp.cmp(tmp.name, "examples/query_result_maf.npz")
 
 
 def test_streaming_query():
-    tmp_output = "/tmp/out.csv"
+    with NamedTemporaryFile(suffix=".csv") as tmp:
 
-    runner = CliRunner()
-    result = runner.invoke(
-        submatrix,
-        [
-            "examples/chr21_partial.h5",
-            "--row-start",
-            14900001,
-            "--row-end",
-            15100001,
-            "-o",
-            tmp_output,
-            "-s",
-        ],
-    )
-    assert result.exit_code == 0, print(result.output)
+        runner = CliRunner()
+        result = runner.invoke(
+            submatrix,
+            [
+                "examples/chr21_partial.h5",
+                "--row-start",
+                14900001,
+                "--row-end",
+                15100001,
+                "-o",
+                tmp.name,
+                "-s",
+            ],
+        )
+        assert result.exit_code == 0, print(result.output)
 
-    assert filecmp.cmp(tmp_output, "examples/query_result.csv")
-
-    os.remove(tmp_output)
+        assert filecmp.cmp(tmp.name, "examples/query_result.csv")
 
 
 def test_streaming_maf_query():
-    tmp_output = "/tmp/maf_out.npz"
+    with NamedTemporaryFile(suffix=".npz") as tmp:
 
-    runner = CliRunner()
-    result = runner.invoke(
-        submatrix_by_maf,
-        ["examples/chr21_partial.h5", "-l", 0.2, "-u", 0.22, "-o", tmp_output, "-s"],
-    )
-    assert result.exit_code == 0, print(result.output)
+        runner = CliRunner()
+        result = runner.invoke(
+            submatrix_by_maf,
+            ["examples/chr21_partial.h5", "-l", 0.2, "-u", 0.22, "-o", tmp.name, "-s"],
+        )
+        assert result.exit_code == 0, print(result.output)
 
-    test_output = np.nan_to_num(sp.load_npz(tmp_output).todense())
-    correct_output = np.nan_to_num(
-        sp.load_npz("examples/query_result_maf.npz").todense()
-    )
+        test_output = np.nan_to_num(sp.load_npz(tmp.name).todense())
+        correct_output = np.nan_to_num(
+            sp.load_npz("examples/query_result_maf.npz").todense()
+        )
 
-    assert np.abs(test_output - correct_output).max() < 0.00001
-
-    os.remove(tmp_output)
+        assert np.abs(test_output - correct_output).max() < 0.00001

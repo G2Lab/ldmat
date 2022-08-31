60eba0a9cb9dc6b37a1f155c28f8baecb1ee7583
Rockwell Weiner <rockwellw@gmail.com>
2022-08-31 13:57:24 -0400

    0.1.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="ldmat",
-    version="0.1.6",
+    version="0.1.7",
     author="Rockwell Weiner",
     author_email="rockwellw@gmail.com",
     description=("Efficient Storage and Querying of Linkage Disequilibrium Matrices"),
